c2ef3c723e915f1562a3ebf98b0561f54f8ff439
wenhzha <53274673+wenhzha@users.noreply.github.com>
2020-09-02 22:56:15 -0700

    Update Home.js

--- app/src/Home.js
+++ app/src/Home.js
@@ -11,7 +11,7 @@ function Home() {
           Welcome to my test site! This site was last modified on 8/30/20 9:38pm
         </p>
         <p>
-          Wenonah test! Please work
+          Wenonah test! Please work! Come on, work!
         </p>
         <a
           className="App-link"
