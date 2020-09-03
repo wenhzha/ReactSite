a7e3b91b96f6886f276f88471234bdafa178513c
wenhzha <53274673+wenhzha@users.noreply.github.com>
2020-09-03 16:35:55 -0700

    Update Home.js

--- app/src/Home.js
+++ app/src/Home.js
@@ -11,7 +11,7 @@ function Home() {
           Welcome to my test site! This site was last modified on 8/30/20 9:38pm
         </p>
         <p>
-          Wenonah test! Please work! Come on, work! What About Now? Last try? :) Work! Okay. what about now? Force windows
+          Wenonah test! Please work! Come on, work! What About Now? Last try? :) Work! Okay. what about now? Force windows, now??
         </p>
         <a
           className="App-link"
