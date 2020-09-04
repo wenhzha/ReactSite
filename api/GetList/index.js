fe8f179ed714d7ff7a6c48b5d550a027eb2f00d1
wenhzha <53274673+wenhzha@users.noreply.github.com>
2020-09-04 13:20:16 -0700

    Update index.js

--- api/GetList/index.js
+++ api/GetList/index.js
@@ -2,7 +2,7 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     context.res = {
-        headers: { 'foobar': 'baz-12:50'},
+        headers: { 'foobar': 'baz-1:20'},
         body: req.headers
     }
 };
